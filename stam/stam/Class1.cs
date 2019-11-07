bd3a4f3c6dd880bc5054bf8c47572dc58b644480
Moshe-atlani <atlani.moshe@gmail.com>
2019-11-07 22:07:04 +0200

    my first commi for the scoole

---
+++ stam/stam/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePourStack
+{
+    class Stack
+    {
+        public Stack()
+        {
+        }
+        public int Pop()
+
+        //מחזירה את הערך שבראש המחסנית ומוציאה אותו
+        //הנחה - המחסנית אותחלה ואינה ריקה
+
+        {
+            return -1;
+        }
+        public int Top()
+
+        //מחזירה את הערך שבראש המחסנית
+        //הנחה - המחסנית אותחלה ואינה ריקה
+
+        {
+            return -1;
+        }
+        public void Push(int x)
+
+        //מוסיפה את הערך x לראש המחסנית
+        //הנחה - המחסנית אותחלה
+
+        {
+        }
+        public bool IsEmpty()
+
+        //מחזירה האם המחסנית ריקה
+        //הנחה - המחסנית אותחלה
+
+        {
+            return false;
+        }
+    }
+}
---
+++ stam/stam/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountEventDigit(12344));
+        }
+        static int Foo(bool b1, bool b2, Stack s)
+        {
+            if (b1 == true && b2 == true)
+            {
+                int x, count = 0;
+                while (!s.IsEmpty())
+                {
+                    x = s.Top();
+                    if (CountDigit(x) / 2 < CountEventDigit(x))
+                        count++;
+                    s.Pop();
+                }
+                return count;
+            }
+            else if (b1 == false && b2 == false)
+            {
+                int x, count = 0;
+                while (!s.IsEmpty())
+                {
+                    x = s.Top();
+                    if (CountDigit(x) / 2 <CountEventDigit(x))
+                        count++;
+                    s.Pop();
+                }
+                return count;
+            }
+            else
+            {
+                int x, count = 0;
+                while (!s.IsEmpty())
+                {
+                    x = s.Top();
+                    if (CountDigit(x) / 2 == CountEventDigit(x))
+                        count++;
+                    s.Pop();
+                }
+                return count;
+            }
+
+        }
+        static int CountEventDigit(int num)
+        {
+            int x, count = 0;
+
+            while (num > 0)
+            {
+                x = num % 10;
+                if (x % 2 == 0)
+                    count++;
+                num /= 10;
+            }
+            return count;
+        }
+        static int CountDigit(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                num = num / 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}
